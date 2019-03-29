187e236f3079e56587cf464405a112ddc64ddd83
jstnf <mcpokeball@gmail.com>
2019-03-28 18:27:34 -0700

    It's alive!

--- src/main/java/com/jstnf/bentoboxtestaddon/Main.java
+++ src/main/java/com/jstnf/bentoboxtestaddon/Main.java
@@ -1,5 +1,18 @@
 package com.jstnf.bentoboxtestaddon;
 
+import world.bentobox.bentobox.api.addons.Addon;
+
 public class Main extends Addon
 {
+	@Override
+	public void onEnable()
+	{
+		getLogger().info("Hello! This is a test BentoBox addon!");
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
 }
